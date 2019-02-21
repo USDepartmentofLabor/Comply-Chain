ba23324d5a721e38b2c62f13d05b93ed644e3b70
LeHaine <colt.daily@gmail.com>
2019-02-21 14:38:09 -0500

    add semantic-ui. add and update scripts to handle building semantic CSS.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Navigator } from "./common/Navigation";
+import "./semantic/dist/semantic.min.css";
 
 class App extends Component {
     render() {
