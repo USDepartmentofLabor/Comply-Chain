5bd11c62d351bc1794ceecf8cb9ccb248213bcac
LeHaine <colt.daily@gmail.com>
2019-02-21 10:56:18 -0500

    add base cordova and react setup.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const CordovaApp = (
+    <HashRouter>
+        <App />
+    </HashRouter>
+);
+const BrowserApp = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const initApp = app => {
+    ReactDOM.render(app, document.getElementById("root"));
+    serviceWorker.register();
+};
+
+if (window.usingCordova) {
+    document.addEventListener(
+        "deviceready",
+        () => {
+            initApp(CordovaApp);
+        },
+        false
+    );
+} else {
+    initApp(BrowserApp);
+}
