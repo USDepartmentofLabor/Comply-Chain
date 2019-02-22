298c42371effb409c295acb0a23552907fce3ab1
LeHaine <colt.daily@gmail.com>
2019-02-22 13:52:55 -0500

    fix LanguageSwitcherContainer to use query params.

--- src/common/Menu/LanguageSwitcher/LanguageSwitcherContainer.js
+++ src/common/Menu/LanguageSwitcher/LanguageSwitcherContainer.js
@@ -1,21 +1,13 @@
-import AbstractLanguageSwitcherContainer from "./AbstractLanguageSwitcherContainer";
-import { strings } from "../../../modules/config/strings";
 import { withRouter } from "react-router-dom";
-import { matchPath } from "react-router";
+import { strings } from "../../../modules/config/strings";
+import AbstractLanguageSwitcherContainer from "./AbstractLanguageSwitcherContainer";
 
 class LanguageSwitcherContainer extends AbstractLanguageSwitcherContainer {
     onLanguageChange = (e, data) => {
         const { history } = this.props;
         const lang = data.value;
         strings.setLanguage(lang);
-
-        const match = matchPath(history.location.pathname, {
-            path: "/:lang/*"
-        });
-        let url = match && match.params && match.params[0];
-        url = "/" + lang + "/" + url;
-
-        history.push(url);
+        history.push(`${history.location.pathname}?lang=${lang}`);
     };
 }
 
