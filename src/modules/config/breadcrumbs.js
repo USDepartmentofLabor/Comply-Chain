a0dd2cf7b73e936bd234106e3596c928bfb3cfcd
LeHaine <colt.daily@gmail.com>
2019-02-26 09:20:39 -0500

    update breadcrumbs.

--- src/modules/config/breadcrumbs.js
+++ src/modules/config/breadcrumbs.js
@@ -5,9 +5,21 @@ const StepBreadcrumb = ({ match }) => {
     return <span>Step {match.params.step}</span>;
 };
 
+const TopicBreadcrumb = ({ match }) => {
+    return <span>Topic {match.params.topic}</span>;
+};
+
 export const breadcrumbs = [
     {
         path: Routes.Step.path,
         breadcrumb: StepBreadcrumb
+    },
+    {
+        path: Routes.Topic.path,
+        breadcrumb: TopicBreadcrumb
+    },
+    {
+        path: `${Routes.Step.path}/topic`,
+        breadcrumb: null
     }
 ];
