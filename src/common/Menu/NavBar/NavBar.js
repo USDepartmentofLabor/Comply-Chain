42acae540267ae904c8157c3a9d9c084391e3982
LeHaine <colt.daily@gmail.com>
2019-02-26 08:29:43 -0500

    fix issue with dropdown menu hiding under Navbar.

--- src/common/Menu/NavBar/NavBar.js
+++ src/common/Menu/NavBar/NavBar.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import SideNav from "../SideNav";
 
 const NavbarWrapper = styled.div`
-    overflow: hidden;
     position: fixed;
     top: 0;
     background-color: #fff;
