7dd10a615fd20bc53414e8d3b8f90b63844ce332
LeHaine <colt.daily@gmail.com>
2019-02-28 10:12:13 -0500

    add components directory the hold basic web elements based on the app theme. add theme.js file to hold global theme variables.

---
+++ src/components/Button/Button.js
@@ -0,0 +1,100 @@
+import styled from "styled-components";
+import { theme } from "../../modules/config/theme";
+import { darken } from "polished";
+
+const borderLine = "2px solid";
+
+const variant = {
+    primary: {
+        color: theme.colors.white,
+        backgroundColor: theme.colors.primary,
+        border: "none",
+        hover: {
+            backgroundColor: darken(0.05, theme.colors.primary),
+            border: "none"
+        },
+        active: {
+            backgroundColor: darken(0.01, theme.colors.primary),
+            border: "none"
+        }
+    },
+    primaryDarker: {
+        color: theme.colors.white,
+        backgroundColor: theme.colors.primaryDarker,
+        border: "none",
+        hover: {
+            backgroundColor: darken(0.05, theme.colors.primaryDarker),
+            border: "none"
+        },
+        active: {
+            backgroundColor: darken(0.01, theme.colors.primaryDarker),
+            border: "none"
+        }
+    },
+    primaryDarkest: {
+        color: theme.colors.white,
+        backgroundColor: theme.colors.primaryDarkest,
+        border: "none",
+        hover: {
+            backgroundColor: darken(0.05, theme.colors.primaryDarkest),
+            border: "none"
+        },
+        active: {
+            backgroundColor: darken(0.01, theme.colors.primaryDarkest),
+            border: "none"
+        }
+    },
+    secondary: {
+        color: theme.colors.primary,
+        backgroundColor: theme.colors.white,
+        border: `${borderLine} ${theme.colors.primary}`,
+        hover: {
+            backgroundColor: theme.colors.white,
+            border: `${borderLine} ${darken(0.05, theme.colors.primary)}`
+        },
+        active: {
+            backgroundColor: theme.colors.white,
+            border: `${borderLine} ${darken(0.3, theme.colors.primary)}`
+        }
+    }
+};
+
+const Button = styled.button`
+    color: ${props => variant[props.variant].color || variant.primary.color};
+    background-color: ${props =>
+        variant[props.variant].backgroundColor ||
+        variant.primary.backgroundColor};
+
+    border: ${props => variant[props.variant].border || variant.primary.border};
+
+    border-radius: 4px;
+    padding: 15px 32px;
+    display: inline-block;
+    font-size: 1em;
+    font-weight: bold;
+
+    svg {
+        vertical-align: middle;
+    }
+
+    &:hover {
+        background-color: ${props =>
+            variant[props.variant].hover.backgroundColor ||
+            variant.primary.hover.color};
+
+        border: ${props =>
+            variant[props.variant].hover.border ||
+            variant.primary.hover.border};
+    }
+
+    &:active {
+        background-color: ${props =>
+            variant[props.variant].active.backgroundColor ||
+            variant.primary.active.backgroundColor};
+
+        border: ${props =>
+            variant[props.variant].active.border ||
+            variant.primary.active.border};
+    }
+`;
+export default Button;
---
+++ src/modules/config/theme.js
@@ -0,0 +1,31 @@
+export const theme = {
+    colors: {
+        primary: "#0071bc",
+        primaryDarker: "#205493",
+        primaryDarkest: "#112e51",
+        base: "#212121",
+        grayDark: "#323a45",
+        grayLight: "#aeb0b5",
+        white: "#fff",
+        primaryAlt: "#02bfe7",
+        primaryAltDarkest: "#046b99",
+        primaryAltDark: "#00a6d2",
+        primaryAltLight: "#9bdaf1",
+        primaryAltLightest: "#e1f3f8",
+        secondary: "#e31c3d",
+        secondaryDarkest: "#981b1e",
+        secondaryDark: "#cd2026",
+        secondaryLight: "#e59393",
+        secondaryLightest: "#f9dede",
+        gold: "#fdb81e",
+        goldLight: "#f9c642",
+        goldLighter: "#fad980",
+        goldLightest: "#fff1d2",
+        green: "#2e8540",
+        greenLight: "#4aa564",
+        greenLighter: "#94bfa2",
+        greenLightest: "#e7f4e4",
+        focus: "#3e94cf",
+        visited: "#4c2c92"
+    }
+};
