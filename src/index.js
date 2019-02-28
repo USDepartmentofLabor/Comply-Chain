2317a0ec5cb55e41d868a7b4fede3f11bd185b18
LeHaine <colt.daily@gmail.com>
2019-02-28 10:54:04 -0500

    clean up code.

--- src/index.js
+++ src/index.js
@@ -21,29 +21,31 @@ const initApp = app => {
     serviceWorker.register();
 };
 
+const addInAppBrowser = () => {
+    window.open = window.cordova.InAppBrowser.open;
+    // prevent's external links from opening in external browser and opens in cordova's InAppBrowser instead.
+    document.onclick = function(e) {
+        e = e || window.event;
+        const element = e.target || e.srcElement;
+
+        if (element.tagName === "A") {
+            const link = element.href;
+            const pattern = /^((http|https):\/\/)/;
+            const pdf = /(\.pdf$)/; // InAppBrowser doesn't support displaying PDF's so we let the system open them.
+            if (pattern.test(link) && !pdf.test(link)) {
+                window.open(link, "_blank");
+                return false;
+            }
+        }
+        return true;
+    };
+};
+
 if (window.cordova) {
     document.addEventListener(
         "deviceready",
         () => {
-            window.open = window.cordova.InAppBrowser.open;
-
-            // prevent's external links from opening in external browser and opens in cordova's InAppBrowser instead.
-            document.onclick = function(e) {
-                e = e || window.event;
-                const element = e.target || e.srcElement;
-
-                if (element.tagName === "A") {
-                    const link = element.href;
-                    const pattern = /^((http|https):\/\/)/;
-                    const pdf = /(\.pdf$)/; // InAppBrowser doesn't support displaying PDF's so we let the system open them.
-                    if (pattern.test(link) && !pdf.test(link)) {
-                        window.open(link, "_blank");
-                        return false;
-                    }
-                }
-                return true;
-            };
-
+            addInAppBrowser();
             initApp(CordovaApp);
         },
         false
