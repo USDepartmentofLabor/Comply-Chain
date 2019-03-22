8d254a434d763835c24b19acdc4a7b791ac5fb08
LeHaine <colt.daily@gmail.com>
2019-03-22 07:13:33 -0400

    Revert "remove unused static data."
    This reverts commit 14bf04cdf1f613e6ca95eaa8caa0d8e0183ae447.

---
+++ src/static/data/step/7/extra/en.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const en = () => {
+    return (
+        <div>
+            <p>
+                As discussed in Basics of a Social Compliance System, your
+                social compliance team has set goals, objectives and targets for
+                your system and periodically assesses performance against those
+                targets. In addition to this internal review, you will also need
+                periodic, <strong>independent review</strong>, which includes{" "}
+                <strong>independent monitoring</strong> and{" "}
+                <strong>independent verification</strong> of the effectiveness
+                of all social compliance program elements.{" "}
+                <em>Note: If, in </em>
+                <Link to="/steps/5">Step 5, Monitor Compliance</Link>
+                <em>
+                    , a company already opted for independent
+                    monitoring/auditing, it is not necessary to repeat this
+                    step, but it is still advised to carry out independent
+                    verification.{" "}
+                </em>
+                Review and assessment by an independent external party helps you
+                identify areas for improvement and provides stakeholders with
+                desired information on the implementation and effectiveness of
+                your system.
+            </p>
+        </div>
+    );
+};
+
+export default en;
---
+++ src/static/data/step/8/extra/es.js
@@ -0,0 +1,57 @@
+import React from "react";
+import OwnWords from "../../../../../components/OwnWords";
+
+const es = () => {
+    return (
+        <div>
+            <p>
+                <OwnWords>
+                    <h2>
+                        <OwnWords.Icon />
+                        <strong>En sus propias palabras</strong>
+                    </h2>
+                    <p>
+                        La responsabilidad de respetar los derechos humanos
+                        exige que las empresas cuenten con pol&iacute;ticas y
+                        procesos para saber y hacer saber que respetan los
+                        derechos humanos en la pr&aacute;ctica. Hacer saber
+                        implica comunicar, ofrecer transparencia y rendir
+                        cuentas a las personas o grupos que puedan verse
+                        afectados y a otros interesados, incluidos los
+                        inversores.
+                    </p>
+                    <p>
+                        La comunicaci&oacute;n puede adoptar diversas formas,
+                        como reuniones personales, di&aacute;logos en
+                        l&iacute;nea, consultas con los afectados e informes
+                        p&uacute;blicos oficiales. Tambi&eacute;n la
+                        informaci&oacute;n oficial est&aacute; evolucionando,
+                        desde los tradicionales informes anuales y los informes
+                        de responsabilidad/sostenibilidad empresarial hasta la
+                        inclusi&oacute;n de actualizaciones en l&iacute;nea e
+                        informes integrados financieros y no financieros.
+                    </p>
+                    <p>
+                        <em>
+                            Fuente:{" "}
+                            <a
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                href="http://www.ohchr.org/Documents/Publications/GuidingPrinciplesBusinessHR_EN.pdf"
+                                title="Principios Rectores sobre las Empresas y los Derechos Humanos de las Naciones Unidas"
+                            >
+                                <i>
+                                    Principios Rectores sobre las Empresas y los
+                                    Derechos Humanos de las Naciones Unidas
+                                </i>
+                            </a>
+                            , 2011.
+                        </em>
+                    </p>
+                </OwnWords>
+            </p>
+        </div>
+    );
+};
+
+export default es;
