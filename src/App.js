7d5ef5af596a582874a2b9621a9ba8d695f53221
LeHaine <colt.daily@gmail.com>
2019-03-29 08:44:53 -0400

    move global CSS styles into styled-component global styles function.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import "./App.css";
 import AppWrapper from "./components/AppWraapper";
 import { LanguageProvider } from "./components/Language";
 import { storage } from "./modules/storage";
 import { isAndroid, isBrowser, isIOS } from "./modules/utils/platform";
 import { Splash } from "./routes";
+import { AppGlobalStyles } from "./AppGlobalStyles";
 
 class App extends Component {
     state = { splashComplete: false };
@@ -14,6 +14,7 @@ class App extends Component {
     render() {
         return (
             <LanguageProvider>
+                <AppGlobalStyles />
                 {isBrowser() && <AppWrapper />}
                 {(isIOS() || isAndroid()) && (
                     <div>
