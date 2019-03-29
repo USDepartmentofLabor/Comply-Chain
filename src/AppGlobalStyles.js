0cc51945f5bab8db602ed8844c5cec85efd73dc3
LeHaine <colt.daily@gmail.com>
2019-03-29 11:05:49 -0400

    update font family prioritization.

--- src/AppGlobalStyles.js
+++ src/AppGlobalStyles.js
@@ -37,7 +37,8 @@ export const AppGlobalStyles = createGlobalStyle`
     body {
         margin: 0;
         padding: 0;
-        font-family: "Source Sans Pro", sans-serif;
+        font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+            "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
         font-weight: 400;
         font-size: 17px;
 
