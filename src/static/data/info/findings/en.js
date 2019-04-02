6d16715b4e0958d68d3123dd90b24207b7e42760
LeHaine <colt.daily@gmail.com>
2019-04-02 11:29:48 -0400

    add findins and goods enlish text content.

--- src/static/data/info/findings/en.js
+++ src/static/data/info/findings/en.js
@@ -3,7 +3,71 @@ import React from "react";
 const en = () => {
     return (
         <div>
-            <h1>Findings en</h1>
+            <h1>Findings on the Worst Forms of Child Labor</h1>
+            <p>
+                The Department of Labor's annual Findings on the Worst Forms of
+                Child Labor focuses on the efforts of certain U.S. trade
+                beneficiary countries and territories to eliminate the worst
+                forms of child labor through legislation, enforcement
+                mechanisms, policies and social programs. Read More
+            </p>
+            The Report presents:
+            <ul>
+                <li>
+                    Findings on the prevalence and sectoral distribution of the
+                    worst forms of child labor in each country.
+                </li>
+                <li>
+                    Country-specific suggestions for government action (since
+                    2009).
+                </li>
+                <li>
+                    Individual country assessments that identify where
+                    Significant, Moderate, Minimal, or No Advancement has been
+                    made (since 2011).
+                </li>
+                <li>
+                    The Report serves as a resource to foreign governments,
+                    NGOs, academics and policymakers working on labor and human
+                    rights issues. It helps inform Congress and Executive Branch
+                    agencies that formulate labor and trade policy and is an
+                    important resource for the Department in assessing future
+                    technical assistance and research priorities as it seeks to
+                    combat child labor around the world.
+                </li>
+            </ul>
+            <p>
+                The Department's Bureau of International Labor Affairs (ILAB)
+                has published the Findings each year since 2002, as mandated by
+                the Trade and Development Act of 2000 (TDA). The TDA requires
+                that countries fulfill commitments to eliminate the worst forms
+                of child labor to be eligible for certain U.S. trade preference
+                programs. It also requires the U.S. Secretary of Labor to issue
+                annual findings on beneficiary country initiatives to implement
+                these commitments.
+            </p>
+            <p>
+                Read more here:{" "}
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.dol.gov/agencies/ilab/resources/reports/child-labor/findings"
+                    title="Child labor Findings Report"
+                >
+                    https://www.dol.gov/agencies/ilab/resources/reports/child-labor/findings
+                </a>
+            </p>
+            <p>
+                Download the 2017 Magazine here:{" "}
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.dol.gov/sites/default/files/images/ilab/ChildLaborReport.pdf"
+                    title="Child labor Findings Report 2017"
+                >
+                    https://www.dol.gov/sites/default/files/images/ilab/ChildLaborReport.pdf
+                </a>
+            </p>
         </div>
     );
 };
