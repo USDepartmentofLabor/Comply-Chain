2ea8d6a425663ef5946ba3b09991d55a1c461c26
LeHaine <colt.daily@gmail.com>
2019-05-09 11:58:49 -0400

    change continue reading title attr to aria-label.

--- src/routes/Search/Search.js
+++ src/routes/Search/Search.js
@@ -398,7 +398,7 @@ class Search extends Component {
                             <SnippetLink
                                 to={this.createSearchQueryUrl(result.to, query)}
                                 target={isBrowser() ? "_blank" : "_self"}
-                                title={`${
+                                aria-label={`${
                                     localizor.strings.general.continueReading
                                 } ${result.title}`}
                             >
