41ed4868fc858731c18c7057985467e3a32f5211
Harpreet <harpreet.singh@zolontech.com>
2020-05-15 13:40:35 -0400

    CCP-48-Step 5: Monitor Compliance - Slides 1-10- ENGLISH

--- src/static/data/step/7/topic/5/en.js
+++ src/static/data/step/7/topic/5/en.js
@@ -131,6 +131,111 @@ const en = () => {
                         country of operation which you could use in interviews
                         with workers where you may have doubts about their age.
                     </p>
+
+                    <h1>How to Interview a Child</h1>
+                    <p>
+                        <em>
+                            &ldquo;Interviewing a child is necessary in connection
+                            with recruitment of young people: a) to assess their
+                            age, b) to assess the maturity in connection with ﬁnding
+                            out what job the person is able to carry out, c)
+                            possible work experience and/or d) if one of the workers
+                            is identified to be underage for the type of job/process
+                            that the child or young person carries out.
+                        </em>
+                    </p>
+                    <p>
+                        <em>
+                            When interviewing a child that you suspect to be below
+                            the minimum required age for the particular type of job
+                            or where the child has been engaged in hazardous work,
+                            it is important to create a trusting and relaxed
+                            atmosphere. It is also important that the conversation
+                            takes place in an undisturbed but not too remote and out
+                            of sight environment. Essentially, the child must feel
+                            as secure as possible.
+                        </em>
+                    </p>
+                    <h2>
+                        <em>What to Observe?</em>
+                    </h2>
+                    <p>
+                        <em>
+                            The child must be treated with respect and recognized as
+                            an individual in his or her own right.
+                        </em>
+                    </p>
+                    <p>
+                        <em>
+                            There must always be two adults present at the interview
+                            at all times. An adult must not spend excessive time
+                            alone with children away from others in order not to
+                            create unnecessary worry for the child and create a
+                            basis for suspicion of inappropriate or threatening
+                            behavior and abuse.
+                        </em>
+                    </p>
+                    <p>
+                        <em>
+                            When interviewing a girl, there must always be a woman
+                            present.
+                        </em>
+                    </p>
+                    <p>
+                        <em>
+                            It is advisable to invite the child&rsquo;s parents or
+                            guardians for the interview.
+                        </em>
+                    </p>
+                    <p>
+                        <em>
+                            If the child is an orphan and no guardian or relatives
+                            can be identified, a representative of a child rights
+                            NGO may be invited.
+                        </em>
+                    </p>
+                    <h2>
+                        <em>During the Interview with a Child</em>
+                    </h2>
+                    <p>
+                        <em>
+                            The key objective of the interview is always to listen
+                            to the child and to try to make an overall assessment of
+                            the child&rsquo;s family and work situation:
+                        </em>
+                    </p>
+                    <div className="indent-margin">
+                        <p>
+                            <em>Reasons why the child works</em>
+                        </p>
+                        <p>
+                            <em>
+                                How does the child contribute to the family income
+                                and what are the circumstances of the family (e.g.
+                                unemployment, disease etc.)?
+                            </em>
+                        </p>
+                        <p>
+                            <em>
+                                How does the child understand and appreciate his or
+                                her own work?&rdquo;
+                            </em>
+                        </p>
+                    </div>
+                    <p>
+                        Source: Save the Children Denmark,{" "}
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="http://www.etiskhandel.no/noop/file.php?id=1580"
+                            title="Child Labour Toolkit, Responsible Approaches to Child Labour in the Textile and Garment Industry"
+                        >
+                            Child Labour Toolkit, Responsible Approaches to Child
+                            Labour in the Textile and Garment Industry
+                        </a>
+                        , 2006.
+                    </p>
+
                     <p>
                         [1] See, Article 9(3) of&nbsp;
                         <a
