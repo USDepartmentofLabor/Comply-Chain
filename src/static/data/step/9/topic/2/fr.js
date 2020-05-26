9e30f5d5c156c1df7d7c1ef5ded297e6cd8d96ec
pbhatt17 <praveenbhatt2000@gmail.com>
2020-05-26 14:01:30 -0400

    step 7 changes

--- src/static/data/step/9/topic/2/fr.js
+++ src/static/data/step/9/topic/2/fr.js
@@ -1,70 +1,212 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import ExampleInAction from "../../../../../../components/ExampleInAction/ExampleInAction";
 import StaticData from "../../../../../../components/StaticData";
+// import manBamboo from "../../../../../images/man_bamboo.jpg";
 
 const fr = () => {
     return (
         <StaticData>
             <div>
-                <h1>Contr&ocirc;le ind&eacute;pendant</h1>
+                <h1>V&eacute;rification ind&eacute;pendante (audit)</h1>
                 <p>
-                    Un contr&ocirc;le ind&eacute;pendant par une tierce partie
-                    est une &eacute;valuation du syst&egrave;me de
-                    conformit&eacute; sociale complet d&rsquo;une entreprise,
-                    afin de d&eacute;terminer s&apos;il est mis en &oelig;uvre
-                    de fa&ccedil;on ad&eacute;quate et minutieuse. Ce
-                    contr&ocirc;le devrait &ecirc;tre ex&eacute;cut&eacute; au
-                    minimum une fois par an.
-                </p>
-                <p>
-                    Les contr&ocirc;leurs devraient &ecirc;tre des organismes de
-                    certification (OC) accr&eacute;dit&eacute;s qui respectent
-                    la norme ISO/CEI&nbsp;17021 ou le guide&nbsp;65 de
-                    l&apos;ISO/CEI, ou d&apos;autres syst&egrave;mes pertinents.
-                    La norme ISO/CEI&nbsp;17021 contient les principes et les
-                    exigences concernant la comp&eacute;tence,
-                    l&apos;uniformit&eacute; et l&apos;impartialit&eacute;
-                    d&apos;un audit et la certification des syst&egrave;mes de
-                    gestion de tous types et des organismes prestataires de ces
-                    activit&eacute;s. Le guide 65 de l&apos;ISO/CEI contient les
-                    exigences g&eacute;n&eacute;rales que doit remplir une
-                    tierce partie offrant un syst&egrave;me de certification
-                    d&apos;un produit ou d&apos;un service pour &ecirc;tre
-                    reconnue comme &eacute;tant comp&eacute;tente et fiable.
-                </p>
-                <p>
-                    Un contr&ocirc;le ind&eacute;pendant devrait inclure des
-                    consultations avec les parties prenantes appropri&eacute;es,
-                    y compris les syndicats lorsqu&apos;ils sont
-                    pr&eacute;sents. Les contr&ocirc;leurs devraient
-                    proc&eacute;der &agrave; des audits t&eacute;moins,
-                    o&ugrave; un contr&ocirc;leur accompagne et observe tout
-                    d&apos;abord les audits r&eacute;alis&eacute;s par les
-                    auditeurs existants de l&apos;entreprise (internes,
-                    ext&eacute;rieurs ou ind&eacute;pendants), avant de
-                    v&eacute;rifier les donn&eacute;es de l&apos;audit pour
-                    confirmer que les syst&egrave;mes de donn&eacute;es de
-                    l&apos;entreprise sont fiables. Les audits t&eacute;moins
-                    annonc&eacute;s sont importants lorsqu&apos;il est
-                    n&eacute;cessaire d&apos;avoir acc&egrave;s &agrave; des
+                    Comme l&rsquo;indique la{" "}
+                    <Link to="/steps/7">
+                        5<sup>e</sup>&nbsp;&eacute;tape: surveiller la
+                        conformit&eacute;
+                    </Link>
+                    , une v&eacute;rification ind&eacute;pendante devrait
+                    comporter des visites inopin&eacute;es sur les lieux de
+                    travail de la cha&icirc;ne d&apos;approvisionnement de
+                    l&apos;entreprise pour &eacute;valuer la conformit&eacute;
+                    avec le code de conduite. Les visites annonc&eacute;es
+                    peuvent &eacute;galement &ecirc;tre utiles lorsqu&apos;il
+                    est n&eacute;cessaire d&apos;avoir acc&egrave;s &agrave; des
                     employ&eacute;s ou &agrave; des documents sp&eacute;cifiques
-                    ; les audits t&eacute;moins inopin&eacute;s peuvent
-                    &eacute;galement &ecirc;tre utiles pour contr&ocirc;ler la
-                    mise en &oelig;uvre appropri&eacute;e des politiques de
-                    l&apos;entreprise. Le contr&ocirc;le devrait
-                    &eacute;galement inclure l&apos;examen des politiques et des
-                    proc&eacute;dures de r&eacute;paration de l&apos;entreprise,
-                    ainsi que des cas pr&eacute;cis de r&eacute;paration, pour
-                    examiner leur &eacute;ventuelle efficacit&eacute;.
+                    sur place. Dans les cas o&ugrave; les auditeurs internes de
+                    la soci&eacute;t&eacute; ou de tierces parties
+                    r&eacute;mun&eacute;r&eacute;es proc&egrave;dent &agrave;
+                    des audits par &eacute;chantillonnage dans les domaines
+                    &agrave; haut risque de la cha&icirc;ne
+                    d&apos;approvisionnement, les sites visit&eacute;s par des
+                    v&eacute;rificateurs ind&eacute;pendants peuvent &ecirc;tres
+                    les m&ecirc;mes que ceux qui ont d&eacute;j&agrave; fait
+                    l&apos;objet d&apos;un audit, ou des sites diff&eacute;rents
+                    dans les m&ecirc;mes domaines, ou les deux.
                 </p>
                 <p>
-                    Les contr&ocirc;leurs devraient fournir &agrave;
-                    l&apos;entreprise un rapport identifiant les points faibles
-                    rep&eacute;r&eacute;s dans le syst&egrave;me de
-                    l&apos;entreprise et dans sa mise en &oelig;uvre. Ils
-                    pourraient &eacute;galement sugg&eacute;rer des mesures de
-                    r&eacute;paration que l&apos;entreprise pourrait prendre
-                    pour r&eacute;gler les points faibles.
+                    Les v&eacute;rificateurs ind&eacute;pendants devraient
+                    &ecirc;tre des personnes ou des organisations
+                    accr&eacute;dit&eacute;es, impartiales, poss&eacute;dant des
+                    connaissances sp&eacute;cialis&eacute;es sur les normes du
+                    travail, des connaissances linguistiques appropri&eacute;es
+                    et des connaissances d&eacute;taill&eacute;es sur les
+                    conditions de travail locales et les pratiques en vigueur
+                    dans l&apos;industrie.
                 </p>
+                <p>
+                    Les v&eacute;rificateurs ind&eacute;pendants devraient
+                    fournir &agrave; l&apos;entreprise un rapport
+                    pr&eacute;sentant leurs conclusions et ils peuvent
+                    recommander des mesures de r&eacute;paration qu&apos;une
+                    entreprise devrait adopter pour r&eacute;gler toute
+                    violation d&eacute;tect&eacute;e. Les entreprises devraient
+                    rendre public ce rapport ou l&apos;inclure dans
+                    l&apos;ensemble de leurs rapports publics.
+                </p>
+                <p>
+                    Dans les situations o&ugrave; il n&rsquo;y a pas
+                    d&rsquo;obligation de rendre des compte &agrave; des
+                    structures institutionnelles, une v&eacute;rification
+                    ind&eacute;pendante peut constituer une autre option pour
+                    assurer la cr&eacute;dibilit&eacute; des r&eacute;sultats de
+                    la v&eacute;rification. Si vous n&apos;&ecirc;tes pas en
+                    mesure d&apos;obtenir une v&eacute;rification
+                    ind&eacute;pendante telle que d&eacute;crite ici, vous
+                    pouvez toutefois conserver des voies de communication
+                    ouvertes avec les parties prenantes qui ont acc&egrave;s aux
+                    site de travail et qui pourraient d&eacute;tecter des abus
+                    des droits des travailleurs. Les syndicats sont
+                    consid&eacute;r&eacute;s par certains comme &eacute;tant des
+                    « v&eacute;rificateurs ind&eacute;pendants »: s&apos;il
+                    existe un syndicat sur le lieu de travail, il devrait
+                    &ecirc;tre au courant des moyens s&ucirc;rs de vous signaler
+                    des probl&egrave;mes. De m&ecirc;me, les travailleurs
+                    individuels et les groupes communautaires peuvent &ecirc;tre
+                    conscients de probl&egrave;mes qui n&apos;ont pas
+                    &eacute;t&eacute; d&eacute;tect&eacute;s par les auditeurs.
+                    Mettre &agrave; la disposition du public une liste des
+                    fournisseurs aupr&egrave;s desquels votre entreprise
+                    s&apos;approvisionne pourra offrir &agrave; ces groupes la
+                    possibilit&eacute; de vous signaler les probl&egrave;mes.
+                </p>
+                <ExampleInAction id="fla">
+                    <h2>
+                        <ExampleInAction.Icon alt="Lightbulb"/>
+                        <strong>
+                            Exemple d&apos;action: Programme
+                            d&apos;accr&eacute;ditation de v&eacute;rificateurs
+                            ext&eacute;rieurs ind&eacute;pendants par la Fair
+                            Labor Association (FLA)
+                        </strong>
+                    </h2>
+                    <p>
+                        Le conseil d&rsquo;administration de la FLA
+                        accr&eacute;dite des organisations de surveillance et
+                        des chefs auditeurs pour son{" "}
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="http://www.fairlabor.org/independent-external-monitoring-reports"
+                            title="Programme de v&eacute;rification ext&eacute;rieure ind&eacute;pendante"
+                        >
+                            programme de v&eacute;rification ext&eacute;rieure
+                            ind&eacute;pendante
+                        </a>{" "}
+                        pour deux ans et cette accr&eacute;ditation est
+                        renouvelable tous les deux ans. Les candidats peuvent
+                        demander leur accr&eacute;ditation pour un pays ou
+                        plusieurs, et pour toutes les dispositions du{" "}
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="http://www.fairlabor.org/our-work/code-of-conduct"
+                            title="Code de conduite de la FLA"
+                        >
+                            code de conduite de la FLA
+                        </a>{" "}
+                        ou pour certaines seulement. Les v&eacute;rificateurs
+                        sont accr&eacute;dit&eacute;s lorsqu&rsquo;ils ont
+                        rempli les crit&egrave;res pr&eacute;cis&eacute;s dans
+                        la charte de la FLA et pour des r&eacute;gions
+                        pr&eacute;cises pour lesquelles ils ont fait la preuve
+                        pratique et th&eacute;orique (par le biais d&apos;audits
+                        parall&egrave;les r&eacute;alis&eacute;s par la FLA au
+                        pr&eacute;alable de l&apos;accr&eacute;ditation) de leur
+                        exp&eacute;rience et de leurs connaissances
+                        sp&eacute;cialis&eacute;es. Les auditeurs en chef sont
+                        des personnes poss&eacute;dant suffisamment
+                        d&apos;exp&eacute;rience pour diriger et administrer une
+                        &eacute;quipe de suivi et pr&eacute;senter les rapports
+                        exig&eacute;s. Tous les v&eacute;rificateurs doivent
+                        faire la d&eacute;monstration de leur
+                        ind&eacute;pendance et de leur impartialit&eacute;, et
+                        d&apos;un tr&egrave;s haut degr&eacute; de rigueur et de
+                        minutie dans l&apos;&eacute;valuation de la
+                        conformit&eacute; dans tous les domaines inclus dans le
+                        code de conduite sur le lieu de travail de la FLA.
+                    </p>
+                </ExampleInAction>
+                <ExampleInAction id="bonsucro">
+                    <h2>
+                        <ExampleInAction.Icon alt="Lightbulb"/>
+                        <strong>
+                            Exemple d&apos;action: Le mod&egrave;le Bonsucro*
+                            d&rsquo;accr&eacute;ditation des organisations de
+                            certification
+                        </strong>
+                    </h2>
+                    <p>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://www.bonsucro.com/en/"
+                            title="Bonsucro"
+                        >
+                            Bonsucro
+                        </a>{" "}
+                        est un r&eacute;seau international de cultivateurs, de
+                        n&eacute;gociants, de groupes de la
+                        soci&eacute;t&eacute; civile, de marques et
+                        d&rsquo;autres parties prenantes qui font la promotion
+                        de la production responsable de la canne &agrave; sucre.
+                        Pour &ecirc;tre reconnue et approuv&eacute;e par
+                        Bonsucro aux fins d&apos;audit et de certification par
+                        rapport &agrave; ses normes, une organisation de
+                        v&eacute;rification, ou selon l&apos;appellation choisie
+                        par Bonsucro, une organisation de certification, doit
+                        d&apos;abord remplir plusieurs crit&egrave;res,
+                        notamment les suivants: 1) une organisation de
+                        certification (OC) doit &ecirc;tre en conformit&eacute;
+                        avec les{" "}
+                        <em>
+                            Exigences 17065:2012 pour les organismes de
+                            certification de produits, processus et services
+                        </em>{" "}
+                        de l&rsquo;Organisation internationale de normalisation
+                        (ISO) et le personnel en charge des audits doit suivre
+                        les directives contenues dans les{" "}
+                        <em>
+                            Lignes directrices ISO 19011:2011 d&rsquo;audit des
+                            syst&egrave;mes de management
+                        </em>
+                        ; 2) une OC doit op&eacute;rer au moins un programme
+                        accr&eacute;dit&eacute; satisfaisant aux crit&egrave;res
+                        de durabilit&eacute; de la norme de production et de la
+                        norme de cha&icirc;ne de contr&ocirc;le de Bonsucro; et
+                        3) une OC est responsable de rester &agrave; jour sur
+                        les exigences de Bonsucro, et, le cas
+                        &eacute;ch&eacute;ant, sur la l&eacute;gislation
+                        europ&eacute;enne applicable. Lorsqu&apos;une OC est
+                        approuv&eacute;e par Bonsucro et qu&apos;elle signe un
+                        accord contractuel, Bonsucro d&eacute;livre une
+                        approbation officielle et place le nom de l&apos;OC sur
+                        son site Web &agrave; titre d&apos;OC officiellement
+                        approuv&eacute;e par Bonsucro. L&rsquo;OC en question
+                        peut ensuite effectuer des audits conform&eacute;ment
+                        aux normes de Bonsucro.
+                    </p>
+                    {/* <img
+                        id="img-browser"
+                        className="small-img"
+                        src={manBamboo}
+                        alt="Homme coupant des bambous"
+                    />{" "}
+                    <p className="small-text">
+                        &copy; Florian Kopp/Alamy Stock Photo
+                    </p> */}
+                </ExampleInAction>
+                
             </div>
         </StaticData>
     );
