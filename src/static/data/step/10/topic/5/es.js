89e62e1b94cd2c768d77bcd0ad56f2a5aa73a949
Harpreet <harpreet.singh@zolontech.com>
2020-06-30 14:11:46 -0400

    CCP-130 Step 8-Topic- Report Performance and Engagement

--- src/static/data/step/10/topic/5/es.js
+++ src/static/data/step/10/topic/5/es.js
@@ -16,6 +16,18 @@ const es = () => {
                     ambos exigen una inversi&oacute;n considerable de tiempo y
                     recursos.
                 </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
                 <ExampleInAction id="nike">
                   <h2>
                       <ExampleInAction.Icon alt="Lightbulb"/>
--- src/static/data/step/10/topic/5/fr.js
+++ src/static/data/step/10/topic/5/fr.js
@@ -18,6 +18,18 @@ const fr = () => {
                     deux exigent un important investissement de temps et de
                     ressources.
                 </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
                 <ExampleInAction id="smallBusiness">
                                     <h2>
                                         <ExampleInAction.Icon alt="Lightbulb"/>
