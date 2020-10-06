177e0d3c5db191e81417b779faa12cc31193b05b
Harpreet <harpreet.singh@zolontech.com>
2020-10-06 11:17:14 -0400

    CCP-170 Step-1 Topic 2 fix

--- src/static/data/step/1/topic/2/es.js
+++ src/static/data/step/1/topic/2/es.js
@@ -5,56 +5,38 @@ const es = () => {
     return (
         <StaticData>
             <div>
-                <h1>Evolución de las normas voluntarias y normas autoritarias</h1>
+                <h1>Los Principios Rectores de las Naciones Unidas sobre las empresas y los derechos humanos, las normas voluntarias y las normas autorizadas cambiantes</h1>
                 <p>
-                    En los &uacute;ltimos a&ntilde;os se intensificaron las
-                    expectativas del p&uacute;blico de que las empresas
-                    controlen las normas del trabajo y los derechos humanos en
-                    las cadenas de suministro. Durante d&eacute;cadas, las
-                    normas de aplicaci&oacute;n voluntaria, como las{" "}
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href="http://www.oecd.org/daf/inv/mne/"
-                    >
-                        L&iacute;neas Directrices de la Organizaci&oacute;n de
-                        Cooperaci&oacute;n y Desarrollo Econ&oacute;micos (OCDE)
-                        para Empresas Multinacionales
-                    </a>{" "}
-                    articularon normas b&aacute;sicas para las empresas en
-                    materia de mano de obra y empleo. La modificaci&oacute;n en
-                    2011 de las L&iacute;neas Directrices de la OCDE, junto con
-                    la adopci&oacute;n en junio de 2011 de los{" "}
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href="https://business-humanrights.org/en/un-guiding-principles"
-                    >
-                        Principios rectores sobre las empresas y los derechos
-                        humanos
-                    </a>{" "}
-                    por parte del Consejo de Derechos Humanos de las Naciones
-                    Unidas desempe&ntilde;aron funciones importantes en este
-                    cambio en las expectativas del p&uacute;blico. De
-                    conformidad con los Principios Rectores sobre empresas y
-                    derechos humanos, los Estados tienen el deber de proteger de
-                    los abusos en materia de derechos humanos en su territorio o
-                    en la jurisdicci&oacute;n de terceros, incluidas las
-                    empresas. Asimismo, los Estados tienen el deber de adoptar
-                    las medidas necesarias para garantizar que en caso de
-                    producirse tales abusos, los afectados tendr&aacute;n acceso
-                    a un recurso efectivo. Por su parte, las empresas son
-                    responsables de respetar los derechos humanos, entre otras
-                    cosas, si no causan ni contribuyen a crear consecuencias
-                    adversas para los derechos humanos con sus propias
-                    actividades y si procuran evitar o atenuar las consecuencias
-                    que guarden una relaci&oacute;n directa con sus operaciones,
-                    productos o servicios por las relaciones comerciales que
-                    tienen entabladas. Los Principios Rectores de las Naciones
-                    Unidas rigen para todos los Estados y todas las empresas,
-                    transnacionales o de otro tipo, independientemente del
-                    tama&ntilde;o, el sector, la ubicaci&oacute;n, los
-                    due&ntilde;os y la estructura.
+                  En los últimos años se intensificaron las expectativas del público de que las empresas controlen las normas del trabajo y los derechos
+                  humanos en las cadenas de suministro. Durante décadas, las normas de aplicación voluntaria, como las&nbsp;
+                  <a
+                         target="_blank"
+                         rel="noopener noreferrer"
+                         href="www.oecd.org/daf/inv/mne/"
+                   >
+                  Líneas Directrices de la Organización de Cooperación y Desarrollo Económicos (OCDE) para Empresas Multinacionales
+                  </a>
+                  &nbsp;articularon normas básicas para las empresas en materia de mano
+                  de obra y empleo. La modificación en 2011 de las Líneas Directrices de la OCDE junto con la adopción en junio de 2011 de los Principios Rectores
+                  de las Naciones Unidas sobre las empresas y los derechos humanos por parte del Consejo de Derechos Humanos de las Naciones Unidas desempeñaron funciones importantes en este cambio en las expectativas del público.
+                </p>
+                <p>
+                De conformidad con los&nbsp;
+                <a
+                       target="_blank"
+                       rel="noopener noreferrer"
+                       href="https://www.ohchr.org/Documents/Publications/GuidingPrinciplesBusinessHR_SP.pdf"
+                 >
+                Principios Rectores sobre las Empresas y los Derechos Humanos
+                </a>
+                , los Estados tienen el deber de proteger a las
+                personas dentro de su territorio o jurisdicción de las violaciones de derechos humanos cometidas por terceros, incluidas las empresas.
+                Asimismo, los Estados tienen el deber de adoptar las medidas necesarias para garantizar que, en caso de producirse abusos, los afectados
+                tengan acceso a recursos efectivos. Por su parte, las empresas tienen la responsabilidad de respetar los derechos humanos, por ejemplo,
+                evitar causar o contribuir a causar un efecto negativo en los derechos humanos como consecuencia de la actividad que realizan. Los actores
+                empresariales también tienen la responsabilidad de brindar a las víctimas acceso a recursos efectivos y de prevenir o mitigar los efectos
+                que guardan un vínculo directo con sus operaciones, productos o servicios por medio de sus relaciones comerciales. Los Principios Rectores
+                de las Naciones Unidas rigen para todos los Estados y todas las empresas, transnacionales o de otro tipo, independientemente del tamaño, del sector al que pertenezcan, de su ubicación, sus dueños y su estructura.
                 </p>
             </div>
         </StaticData>
