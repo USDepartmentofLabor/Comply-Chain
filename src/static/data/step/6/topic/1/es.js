7c60c2d23007eed8f3a4deee1cf700abf9edb211
pbhatt17 <praveenbhatt2000@gmail.com>
2020-10-09 14:19:57 -0400

    step 4 spanish changes

--- src/static/data/step/6/topic/1/es.js
+++ src/static/data/step/6/topic/1/es.js
@@ -7,38 +7,58 @@ const es = () => {
             <div>
                 <h1>Comunicaci&oacute;n, no vigilancia</h1>
                 <p>
-                    En las &uacute;ltimas dos d&eacute;cadas, muchas empresas
-                    descubrieron por experiencia propia que limitarse a auditar
-                    los lugares de trabajo y remediar las infracciones no
-                    resuelve muchos de los problemas laborales que se dan en las
-                    plantas de producci&oacute;n. La capacitaci&oacute;n, el
-                    fortalecimiento de capacidades y un &eacute;nfasis en la
-                    mejora continua han demostrado ser la manera m&aacute;s
-                    eficaz de continuar avanzando hacia un mayor grado de
-                    cumplimiento de los c&oacute;digos de conducta. Entablar
-                    relaciones a largo plazo con proveedores y dem&aacute;s
-                    partes interesadas clave es el mejor punto de partida para
-                    este tipo de fortalecimiento de capacidades y mejora
-                    continua.
+                    En las &uacute;ltimas dos d&eacute;cadas, muchas empresas descubrieron por experiencia propia que limitarse 
+                    a auditar los lugares de trabajo y remediar las infracciones no resuelve muchos de los problemas laborales que 
+                    se dan en las plantas de producci&oacute;n. La capacitaci&oacute;n, el fortalecimiento de capacidades y un 
+                    &eacute;nfasis en la mejora continua han demostrado ser la manera m&aacute;s eficaz de continuar avanzando 
+                    hacia un mayor grado de cumplimiento de los c&oacute;digos de conducta. Entablar relaciones a largo plazo con 
+                    proveedores y dem&aacute;s partes interesadas clave es el mejor punto de partida para este tipo de fortalecimiento 
+                    de capacidades y mejora continua.
                 </p>
                 <p>
-                    A lo largo de todas las secciones de la presente
-                    aplicaci&oacute;n encontrar&aacute; distintas formas de
-                    comunicarse con las partes interesadas. En esta
-                    secci&oacute;n se hace hincapi&eacute; en dos formas
-                    espec&iacute;ficas de comunicaci&oacute;n:{" "}
-                    <strong>
-                        la capacitaci&oacute;n y el fortalecimiento de
-                        capacidades
-                    </strong>
-                    , que deber&iacute;an hacerse extensivas a todas las partes
-                    responsables de hacer funcionar el sistema de cumplimiento
-                    social, sin olvidar las entidades en la cadena de
-                    suministro. Adem&aacute;s, contempla opciones para los{" "}
-                    <strong>mecanismos de reclamaci&oacute;n </strong>mediante
-                    los cuales trabajadores, defensores y comunidades pueden
-                    plantear a la empresa problemas de derechos humanos sin
-                    temor a represalias.
+                    A lo largo de todas las secciones de la aplicaci&oacute;n encontrar&aacute; distintas formas de comunicarse con 
+                    las partes interesadas. En esta secci&oacute;n se hace hincapi&eacute; en dos formas espec&iacute;ficas de 
+                    comunicaci&oacute;n: <strong>la capacitaci&oacute;n y el fortalecimiento de capacidades,</strong> que deber&iacute;an 
+                    hacerse extensivas a todas las partes responsables de hacer funcionar el sistema de cumplimiento social, sin olvidar 
+                    las entidades en la cadena de suministro. Adem&aacute;s, contempla opciones para los mecanismos de reclamaci&oacute;n 
+                    mediante los cuales trabajadores, defensores y comunidades pueden plantear a la empresa problemas de derechos humanos 
+                    sin temor a represalias.
+                </p>
+                <p>
+                    Para comenzar a hablar de estos temas, es importante explorar toda la gama de puntos de ingreso al di&aacute;logo que 
+                    se presentan en la cadena de suministro. ILAB descubri&oacute; que la seguridad y salud en el trabajo (OSH) son un buen 
+                    punto de ingreso para que las empresas comiencen a dialogar con los proveedores sobre el tema de los derechos humanos y 
+                    condiciones aceptables de trabajo. Tras analizar las evaluaciones program&aacute;ticas de los proyectos dirigidos a 
+                    reducir el trabajo infantil y el trabajo forzoso en las cadenas de suministro a la luz de ILAB, en parte mediante la 
+                    participaci&oacute;n de los empleadores, ILAB concluy&oacute; que la OSH se percibe como un tema t&eacute;cnico y no 
+                    conflictivo dado que las partes interesadas ven mejoras que benefician tanto a empleadores como a trabajadores.
+                </p>
+
+                <p>
+                    Por ejemplo, en M&eacute;xico, en el contexto del proyecto{" "} 
+                    <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.dol.gov/agencies/ilab/campos-de-esperanza-fields-hope"
+                    title="Campos de Esperanza"
+                    >
+                        Campos de Esperanza
+                    </a>{" "}ejecutado por World Vision* se 
+                    descubri&oacute; que los representantes de las asociaciones de ca&ntilde;a de az&uacute;car y de empleadores en las 
+                    instancias local y nacional hab&iacute;an expresado la clara necesidad de poder contar con los datos de un an&aacute;lisis 
+                    costo-beneficio para justificar la introducci&oacute;n de mejoras en el lugar de trabajo. Sin embargo, incluso sin 
+                    contar a&uacute;n con esa informaci&oacute;n, en la{" "}
+                    <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.dol.gov/sites/dolgov/files/ILAB/MexicoCdE_meval.pdf"
+                    title="evaluaci&oacute;n de mitad de per&iacute;odo"
+                    >
+                        evaluaci&oacute;n de mitad de per&iacute;odo
+                    </a>{" "}se descubri&oacute; que 
+                    el sector de la ca&ntilde;a de az&uacute;car a nivel nacional desempe&ntilde;aba una funci&oacute;n clave para respaldar 
+                    a los promotores de OSH del proyecto en los ingenios azucareros como primer paso para mejorar las condiciones en el lugar 
+                    de trabajo.
                 </p>
             </div>
         </StaticData>
--- src/static/data/step/6/topic/2/es.js
+++ src/static/data/step/6/topic/2/es.js
@@ -7,18 +7,14 @@ const es = () => {
             <div>
                 <h1>¿A qui&eacute;nes se debe capacitar?</h1>
                 <p>
-                    Muchas personas y organismos desempe&ntilde;an alguna
-                    funci&oacute;n para que funcione el sistema de cumplimiento
-                    social de la empresa, y cada una de esas funciones es
-                    esencial. Cuanto m&aacute;s se comprendan las funciones y
-                    las responsabilidades de cada uno, mejor funcionar&aacute;
-                    el sistema. Adem&aacute;s de capacitar a los empleados
-                    responsables del cumplimiento social, la empresa debe tener
-                    en cuenta a las siguientes partes de su cadena de
-                    suministro:
+                    Muchas personas y organismos desempe&ntilde;an alguna funci&oacute;n para que funcione el sistema de 
+                    cumplimiento social de la empresa, y cada una de esas funciones es esencial. Cuanto m&aacute;s se 
+                    comprendan las funciones y las responsabilidades de cada uno, mejor funcionar&aacute; el sistema. 
+                    Adem&aacute;s de capacitar a los empleados responsables del cumplimiento social, la empresa debe tener 
+                    en cuenta a las siguientes partes de su cadena de suministro:
                 </p>
                 <ul>
-                    <li>Sus empleados</li>
+                    <li>Los empleados</li>
                     <li>Los contratistas o agentes, si procede</li>
                     <li>
                         Los proveedores en los distintos estratos de la cadena
