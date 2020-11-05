d1bd8354805f2668a1bcae7af56f6139c25298d8
Harpreet <harpreet.singh@zolontech.com>
2020-11-05 13:37:02 -0500

    CCP-201 Malay content changes - Basics

---
+++ src/static/data/step/2/topic/1/ms.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import StaticData from "../../../../../../components/StaticData";
+
+const ms = () => {
+    return (
+        <StaticData>
+            <div>
+                <h1>Model Sistem Pematuhan Sosial</h1>
+                                <p>
+                                Walaupun sistem mungkin berbeza-beza dari industri ke industri, sistem pematuhan sosial yang baik di mana industri merangkumi pelbagai komponen, berfungsi secara bersepadu.
+                                </p>
+                                <p>
+                                    Walaupun langkah-langkah di bawah ini dihitung untuk kemudahan fahaman pematuhan sosial adalah proses berulang dan berterusan.
+                                    Syarikat boleh memilih untuk meneruskan langkah-langkah ini dalam urutan yang berbeza, dan biasanya tidak boleh mengharapkan
+                                    untuk menyelesaikan satu langkah sebelum melakukan yang lain. Pada masa yang sama, sistem tanpa semua komponen misalnya, sistem
+                                    audit yang beroperasi secara terpisah dari komunikasi dan latihan, pemulihan dan langkah-langkah lain kemungkinan besar tidak
+                                    mencukupi untuk menangani masalah buruh yang mencabar yang boleh timbul dalam rantai bekalan global.
+                                </p>
+                                <ol>
+                                    <li>
+                                        <em>
+                                            <strong>Libatkan</strong>
+                                        </em>
+                                        <em>  pihak berkepentingan dan rakan kongsi</em>
+                                    </li>
+                                    <li>
+                                        <strong>
+                                            <em>Kaji</em>
+                                        </strong>
+                                        <em> risiko dan impak</em>
+                                    </li>
+                                    <li>
+                                        <strong>
+                                            <em>Bangunkan</em>
+                                        </strong>
+                                        <em> kod tingkah laku</em>
+                                    </li>
+                                    <li>
+                                        <strong>
+                                            <em>Berkomunikasi dan Berlatih</em>
+                                        </strong>
+                                        <em> di sepanjang rantai bekalan anda</em>
+                                    </li>
+                                    <li>
+                                        <strong>
+                                            <em>Pantau </em>
+                                        </strong>
+                                        <em>pematuhan</em>
+                                    </li>
+                                    <li>
+                                        <strong>
+                                            <em>Batasi </em>
+                                        </strong>
+                                        <em>pelanggaran</em>
+                                    </li>
+                                    <li>
+                                        <strong>
+                                            <em>Kaji semula secara bebas </em>
+                                        </strong>
+                                    </li>
+                                    <li>
+                                        <strong>
+                                            <em>Lapor</em>
+                                        </strong>
+                                        <em> prestasi dan&nbsp;</em>
+                                        <strong>
+                                           <em>penglibatan</em>
+                                        </strong>
+                                    </li>
+                                </ol>
+                                <p>
+                                    Penting untuk diperhatikan bahawa kebanyakan sistem pematuhan sosial
+                                    yang mantap dirancang untuk menangani piawaian buruh asal yang dikenal
+                                    pasti oleh Pertubuhan Buruh Antarabangsa (ILO)―meliputi kebebasan berpersatuan,
+                                    perundingan kolektif, diskriminasi dalam pekerjaan, buruh kanak-kanak dan
+                                    buruh paksa ― dan juga standard buruh seperti keselamatan dan kesihatan di
+                                    tempat kerja, gaji dan waktu bekerja (lihat{" "}
+                                    <Link to="/steps/5">Step 3,  Develop a Code of Conduct</Link>
+                                    &nbsp;untuk maklumat lanjut mengenai standard ILO ini).
+                                </p>
+                                <p>
+                                    Aplikasi ini memberikan panduan untuk sistem yang komprehensif tetapi memberi
+                                    tumpuan khusus kepada buruh kanak-kanak dan buruh paksa, sesuai dengan mandat
+                                    yang diberikan kepada ILAB di bawah Akta Membenarkan Semula Perlindungan Mangsa Perdagangan (TVPRA) 2005.
+                                </p>
+             </div>
+        </StaticData>
+    );
+};
+
+export default ms;
