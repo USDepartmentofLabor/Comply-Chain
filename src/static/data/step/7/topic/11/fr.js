9fde3b144fe7ca40882fc35fb2e603eaa2b5fe9d
vikramkm114a <68656298+vikramkm114a@users.noreply.github.com>
2022-03-02 10:30:52 -0500

    removed and added grievance mechanism from step 4 to step 5 pt 2

---
+++ src/static/data/step/7/topic/11/fr.js
@@ -0,0 +1,19 @@
+import React from "react";
+import StaticData from "../../../../../../components/StaticData";
+
+
+const fr = () => {
+    return (
+        <StaticData>
+            <div>
+                <h2>Reaching Beyond Tier 1</h2>
+                <p> 
+                    Place holder
+                </p>
+                
+            </div>
+        </StaticData>
+    );
+};
+
+export default fr;
