d07a7c61f1be2ba98afbfa8ee8a69a13c3595aa9
pbhatt17 <praveenbhatt2000@gmail.com>
2022-04-07 15:44:43 -0400

    fixing bookmark issue for topic

--- src/routes/Topic/components/TopicView/TopicView.js
+++ src/routes/Topic/components/TopicView/TopicView.js
@@ -147,8 +147,8 @@ class TopicView extends Component {
                     items = {
                         title: stepData.topics[topic - 1].title,
                         stepTitle: stepData.title,
-                        titleString: `other_steps.0.topics.${topic - 1}.title`,
-                        stepTitleString: `other_steps.0.title`,
+                        titleString: `other_steps.1.topics.${topic - 1}.title`,
+                        stepTitleString: `other_steps.1.title`,
                         topicData: stepData.topics[topic - 1].content,
                         prevStep: prevStep && `/othersteps/${prevStep}`,
                         prevTopic: prevTopic && `/othersteps/${step}/topic/${prevTopic}`,
