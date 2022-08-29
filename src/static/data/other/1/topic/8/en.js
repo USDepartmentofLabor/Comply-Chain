55dd85d02181cf3cf6414c79341e8544903a010c
pbhatt17 <praveenbhatt2000@gmail.com>
2022-08-29 17:17:53 -0400

    Why Develop a Social Compliance System? content changes

--- src/static/data/other/1/topic/8/en.js
+++ src/static/data/other/1/topic/8/en.js
@@ -10,7 +10,12 @@ const en = () => {
                 <h1>
                     <strong>Employee recruitment and retention</strong>
                 </h1>
+                
                 <p>
+                Social compliance is linked with improved personnel recruitment, engagement, and retention.
+                </p>
+                <p>
+                    <em> Recruiting talented workers:</em>
                     A growing body of&nbsp;
                     <a
                         target="_blank"
@@ -19,18 +24,86 @@ const en = () => {
                     >
                     research
                     </a>
-                    &nbsp;shows that employees,
-                    particularly younger ones, increasingly prefer to work for
-                    companies that are socially and environmentally responsible,
-                    and factor this into future employment decisions.
+                    &nbsp;shows that employees, particularly younger ones, increasingly{" "}
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.ipsos.com/sites/default/files/publication/1970-01/loyalty-erm-engaging-employees-through-corporate-responsibility.pdf"
+                        title="prefer to work for companies"
+                    >
+                    prefer to work for companies
+                    </a>{" "}
+                    
+                    that are socially and environmentally responsible, and many factor this into future employment decisions. Businesses 
+                    that develop robust social compliance systems are also more likely to recruit workers fairly and in accordance with 
+                    international labor standards across their supply chains. The creation of a strong enabling environment that provides 
+                    workers with a collective voice may also make these companies a more attractive choice for prospective employees, 
+                    particularly in countries with weaker labor enforcement.
+                </p>
+                <p>
+                    <em>Worker engagement:</em> Delayed wage payments, unaddressed grievances, and the hinderance of workers&rsquo; right to collectively 
+                    organize can adversely impact employee motivation and contradict fundamental principles and rights at work. Companies 
+                    that implement social compliance and corporate sustainability systems report stronger employee motivation, engagement, 
+                    and performance. 
+                </p>
+                <p>
+                    <em>Workforce turnover and retention:</em>{" "}
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.frontiersin.org/articles/10.3389/fpsyg.2016.00704/full"
+                        title="Higher rates of employee turnover"
+                    >
+                    Higher rates of employee turnover
+                    </a>
+                     {" "}
+                    are closely linked with social compliance 
+                    problems like poor working conditions and discriminatory policies. Research shows that companies with stronger social 
+                    compliance practices often retain their staff longer and reduce turnover rates.
                 </p>
                 <OwnWords>
                     <h2>
                         <OwnWords.Icon alt="Feedback"/> <strong>In Their Own Words</strong>
-                        {": "}
+                        {": "} Building a strong workforce through purpose and compliance in Michigan&rsquo;s energy sector 
                     </h2>
                     <p>
-                        Placeholder
+                        &rdquo;Widely viewed as an &rdquo;employer of choice&rdquo; in the region, DTE&rsquo;s* historic and successful approach 
+                        to recruiting the right talent at the right time was focused on attracting the best qualified talent, screening and 
+                        testing to achieve job and cultural fit, and targeted outreach strategies to ensure that compliance-related goals were met.  
+                    </p>
+                    <p>
+                        Today, DTE&rsquo;s approach to talent acquisition has changed. Our work to lead with our purpose, create a culture of 
+                        service, and build a diverse and inclusive workforce, has influenced our hiring strategy.  We&rsquo;re also looking for skills 
+                        and expertise &mdash; in engineering, technology and skilled trades &mdash; that are in high demand and critical to our industry, while 
+                        the labor market is tightening and shrinking.&rdquo;
+                    </p>
+                    <p>
+                        &rdquo;At DTE Energy, we&rsquo;re focused on building a safe environment where all employees feel welcome and a sense of 
+                        belonging.  DTE&rsquo;s caring and purpose-driven culture is what makes us unique. We recognize our employees for being safe, 
+                        caring and providing exceptional service to each other and our customers. We also live our purpose: “We serve with our energy, 
+                        the lifeblood of communities and the engine of progress.” We establish and reinforce these values and behaviors to employees 
+                        from the moment they&rsquo;re hired through onboarding and subsequent training, communication and engagement activities to ensure 
+                        our employees actively contribute to a more diverse, equitable and inclusive culture.&rdquo;
+                    </p>
+                    <p>
+                        &rdquo;DTE Energy is committed to offering compensation that is competitive, market-driven and internally equitable. To ensure 
+                        this, DTE conducts an annual review of compensation practices as part of its affirmative action programs. Approximately half 
+                        of DTE Energy&rsquo;s employees are represented by unions, where pay is uniformly determined through contracts regardless of an 
+                        employee&rsquo;s gender. For non-represented employees, DTE&rsquo;s Human Resources professionals establish pay ranges for each job 
+                        classification, and then work with hiring leaders to make competitive offers within the range to candidates based on objective 
+                        factors like years of experience and strength of skills relevant to the job.&rdquo; 
+                    </p>
+                    <p>
+                        Source:{" "}
+                        <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://empoweringmichigan.com/human-capital-management/"
+                        title="https://empoweringmichigan.com/human-capital-management/"
+                    >
+                    https://empoweringmichigan.com/human-capital-management/
+                    </a>
+                        
                     </p>
                 </OwnWords>
             </div>
--- src/static/data/other/1/topic/9/en.js
+++ src/static/data/other/1/topic/9/en.js
@@ -13,32 +13,139 @@ const en = () => {
                 </h1>
                 <p>
                     For some companies, having a social compliance system is
-                    intrinsically &ldquo;the right thing to do.&rdquo; Such
-                    companies are more likely to devote significant support and
-                    resources to their social compliance systems.
+                    intrinsically &ldquo;the right thing to do.&rdquo; Such companies are more likely to devote significant support and 
+                    resources to their social compliance systems, often in parallel with community-based programming, which have an overall 
+                    benefit for the communities in which they operate.
                 </p>
-                <ExampleInAction id="better-work-garment">
-                                    <h4>
-                                        <ExampleInAction.Icon alt="Lightbulb"/>
-                                        <strong>Example in Action</strong>
-                                        {": "}
-                                        <em>
-                                            <strong>Placeholder</strong>
-                                        </em>{" "}
-                                    </h4>
-                                    <p>Placeholder</p>
+                <ExampleInAction id="cocacola-globalgoal">
+                <h4>
+                <ExampleInAction.Icon alt="Lightbulb"/>
+                <strong>Example in Action</strong>
+                    {": "} Coca-Cola&rsquo;s &rdquo;5by20&rdquo; global goal
+                </h4>
+                <p>
+                    The Coca-Cola Company*, a multinational corporation that operates globally, identified{" "}
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.coca-colacompany.com/sustainability/our-priority-sustainability-issues"
+                        title="19 priority sustainability issues"
+                        >
+                       19 priority sustainability issues
+                    </a>{" "}
+                        impacting their business including human rights and workplace conditions. Coca-Cola works towards this 
+                        priority through a Human Rights Policy, auditing thousands of suppliers to validate compliance, and engaging 
+                        third parties to evaluate labor rights risks, such as risks present in the sugar supply chain. Coca-Cola 
+                        notes that investing in social compliance, such as ensuring inclusivity in the workplace, is &rdquo;both the right 
+                        thing to do and a strategic business priority&rdquo;. 
+                    </p>
+                    <p>
+                        To that end, their sustainability initiatives include both compliance and social programs that address structural 
+                        inequalities, including programs that address barriers that women face around the world. In 2010, Coca-Cola announced their 
+                        &rdquo;5by20&rdquo; global initiative at the UN Headquarters in New York City. This initiative aimed to facilitate the 
+                        economic empowerment of 5 million women entrepreneurs across their supply chains by 2020. 
+                    </p>
+                    <p>
+                        Coca-Cola worked directly with the UN and ensured their efforts aligned with the UN&rsquo;s Millennium Development Goals 
+                        and the Women&rsquo;s Empowerment Principles. Over the next 10 years, Coca-Cola worked on increasing access to economic 
+                        opportunities for women, breaking down barriers women experience in the workplace, and empowering women through professional 
+                        development courses. At the end of the 10-year initiative, Coca-Cola not only met their target of five million women 
+                        entrepreneurs but surpassed it. Their achievements included providing 4,842,777 million women with business skills training, 
+                        1,767,796 million women with networking or mentoring opportunities, and 1,690,581 million women with access to finance or assets. 
+                    </p>
+                    <p>
+                        Summarizing these accomplishments, Coca-Cola Chairman and CEO James Quincy said, “By investing in women&rsquo;s economic 
+                        empowerment over the past decade, we have created shared value in hopes of a better shared future—enabling improved livelihoods 
+                        for women, their families, and their communities.” 
+                    </p>
+                    <p>
+                        Read more about Coca-Cola&rsquo;s &rdquo;5by20&rdquo; goal here:{" "}
+                        <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.coca-colacompany.com/content/dam/journey/us/en/policies/pdf/sustainability/coca-cola-5by20-report-march-2021.pdf"
+                        title="https://www.coca-colacompany.com/content/dam/journey/us/en/policies/pdf/sustainability/coca-cola-5by20-report-march-2021.pdf"
+                        >
+                       https://www.coca-colacompany.com/content/dam/journey/us/en/policies/pdf/sustainability/coca-cola-5by20-report-march-2021.pdf
+                        </a>
+                        . 
+                    </p>
+                    <p>
+                        Read more about Coca-Cola&rsquo;s social compliance and sustainability work here:{" "}
+                        <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.coca-colacompany.com/content/dam/journey/us/en/reports/coca-cola-business-environmental-social-governance-report-2020.pdf"
+                        title="https://www.coca-colacompany.com/content/dam/journey/us/en/reports/coca-cola-business-environmental-social-governance-report-2020.pdf"
+                        >
+                       https://www.coca-colacompany.com/content/dam/journey/us/en/reports/coca-cola-business-environmental-social-governance-report-2020.pdf
+                        </a>
+                        
+                    </p>
                     </ExampleInAction>
+
                     <ExampleInAction id="better-work-garment">
-                                    <h4>
-                                        <ExampleInAction.Icon alt="Lightbulb"/>
-                                        <strong>Example in Action</strong>
-                                        {": "}
-                                        <em>
-                                            <strong>Placeholder</strong>
-                                        </em>{" "}
-                                    </h4>
-                                    <p>Placeholder</p>
-                                </ExampleInAction>            
+                        <h4>
+                        <ExampleInAction.Icon alt="Lightbulb"/>
+                        <strong>Example in Action</strong>
+                        {": "}Hindustan Unilever&rsquo;s* fair compensation framework
+                        </h4>
+                        <p>
+                            Hindustan Unilever&rsquo;s operations are primarily based in India, where millions of people face extreme levels of rural poverty. 
+                            As part of its social compliance system, Hindustan Unilever made a conscious effort to ensure that all workers within their operations, 
+                            from the factory floor to office workers, received wages that were well above the statutory minimum. Such efforts are integrated into 
+                            Unilever&rsquo;s Responsible Sourcing Policy, which aims to make &rdquo;a positive social impact on the lives of millions of people in our 
+                            supply chains around the world.&rdquo; Their approach is to ensure implementation of mandatory requirements among their suppliers while also 
+                            advancing good practices above and beyond minimum requirements. 
+                        </p>
+                        <p>
+                            Unilever&rsquo;s{" "}
+                            <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://www.hul.co.in/planet-and-society/raise-living-standards/"
+                            title="fair compensation framework "
+                            >
+                            fair compensation framework 
+                            </a>
+                            {" "}
+                            is one such good practice. Described as a &rdquo;living wage,&rdquo; these wages considered market indicators such as inflation, commodity prices, 
+                            gender disparities, and standards of living. This commitment to a living wage ensured that employees would be able to provide not just 
+                            for themselves but their families as well. 
+                        </p>
+                        <p>
+                            Along with this promise to their employees linked with their social compliance efforts, Hindustan Unilever also sought to empower 
+                            female entrepreneurs through Project Shakti, a program that supported rural women in villages across India by cultivating 
+                            entrepreneurial skills. Hindustan Unilever provided training to women in rural communities on topics including communication, 
+                            negotiation, marketing, distribution management, and accounting. In addition to skills training, all the women who participated 
+                            in the training were provided with the opportunity to sell products to small retail outlets in their vicinity in order to help 
+                            them establish additional sources of income. As of the end of 2020, Project Shakti trained more than 136,000 women entrepreneurs 
+                            across 18 states and plans to extend this project into Sri Lanka, Pakistan, Ethiopia, Egypt, and Colombia.
+                        </p>
+                        <p>
+                        For more information about Hindustan Unilever&rsquo;s fair compensation framework, visit:{" "}
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://assets.unilever.com/files/92ui5egz/qa/93055db1df9d3c3f24e88c3f72ee841d90fa7700.pdf/responsible-sourcing-policy-interactive-final.pdf"
+                            title="https://assets.unilever.com/files/92ui5egz/qa/93055db1df9d3c3f24e88c3f72ee841d90fa7700.pdf/responsible-sourcing-policy-interactive-final.pdf"
+                            >
+                            https://assets.unilever.com/files/92ui5egz/qa/93055db1df9d3c3f24e88c3f72ee841d90fa7700.pdf/responsible-sourcing-policy-interactive-final.pdf 
+                        </a>
+                        
+                        </p>
+                        <p>
+                        For more information on Project Shakti, visit:{" "}
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://www.hul.co.in/planet-and-society/case-studies/enhancing-livelihoods-through-project-shakti/"
+                            title="https://www.hul.co.in/planet-and-society/case-studies/enhancing-livelihoods-through-project-shakti/"
+                            >
+                            https://www.hul.co.in/planet-and-society/case-studies/enhancing-livelihoods-through-project-shakti/ 
+                        </a>. 
+                        </p>
+                    </ExampleInAction>            
 
             </div>
         </StaticData>
