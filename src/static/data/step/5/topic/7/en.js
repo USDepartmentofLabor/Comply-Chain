950b8cbcb3220255445534f35bf7c60ff0688a4e
Gnanendra18 <tognanendrakumar@gmail.com>
2022-09-09 22:51:30 +0530

    Step 3: Develop a Code of Conduct changes

--- src/static/data/step/5/topic/7/en.js
+++ src/static/data/step/5/topic/7/en.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import StaticData from "../../../../../../components/StaticData";
 
 const en = () => {
@@ -7,44 +6,11 @@ const en = () => {
         <StaticData>
             <div>
                 <h1>Communicating the Code</h1>
-                <p>
-                    Establishing a code is essential to broadcasting a company&rsquo;s essential compliance priorities and objectives and ensuring that a 
-                    company&rsquo;s operations adhere to and promote internationally recognized labor rights. However, key supply chain partners may not 
-                    fully understand or implement the code. Effective communication is an important aspect of any social compliance program. Many companies 
-                    have learned that merely auditing workplaces and remediating violations alone does not solve many of the labor problems found at harvesting, 
-                    mining, and production sites; processing facilities; and other workplaces. Ongoing training and communication, capacity-building, and an 
-                    emphasis on continuous improvement have been found to be the most effective path to sustaining progress toward greater compliance.
-                </p>
-                <p>
-                    Social compliance policies must be backed with mechanisms to ensure leadership, employees, agents, and suppliers understand, apply, and 
-                    adhere to the expected standard. Without providing proper guidance and training to supply chain partners, companies run the risk of higher 
-                    compliance costs throughout their operations and supply chain. This lack of guidance and training may elevate the risk of child labor and 
-                    forced labor entering corporate supply chains.
-                </p>
-                <p>
-                    Many of the world&rsquo;s leading companies have developed robust communication strategies that disseminate the code, integrate the code 
-                    into supplier contracts, and ensure comprehension. This is particularly important for companies that have multiple tiers of suppliers, as 
-                    second-, third-, and fourth-tier suppliers often lack the material or institutional capacity to implement key provisions of a code.
-                </p>
-
-                <p>
-                    <Link to="/steps/4">
-                        Step 4, Communicate and Train Across Your Supply Chain
-                    </Link>{" "}
-                    discusses the ins and outs of where, when, and how to communicate your code and compliance system to a variety of audiences: your own 
-                    employees, your shareholders, your suppliers, supplier managers and supervisors, workers at facilities throughout the supply chain, 
-                    governments, workers&rsquo; organizations, community groups, and others. It will also discuss ways to make the code more accessible to 
-                    stakeholders by avoiding unnecessary technical jargon and providing for translation into multiple languages. Effective communication 
-                    and training throughout the supply chain is key to building a culture of compliance.
-                </p>
-                <p>
-                    <em>
-                        * References to specific companies and non-governmental entities within this guide are for informational purposes only to demonstrate 
-                        best practices that have been recognized internationally, as of the app&rsquo;s release date. Referencing the entities should not be 
-                        interpreted as an official endorsement of those entities, their products, or services by the U.S. Department of Labor and their 
-                        inclusion should not be interpreted as a comprehensive review of these entities&rsquo; practices in all areas.
-                    </em>
-                </p>
+                <p>Establishing a code is essential to broadcasting a company&rsquo;s essential compliance priorities and objectives and ensuring that a company&rsquo;s operations adhere to and promote internationally recognized labor rights. However, key supply chain partners may not fully understand or implement the code. Effective communication is an important aspect of any social compliance program. Many companies have learned that merely auditing workplaces and remediating violations alone does not solve many of the labor problems found at harvesting, mining, and production sites; processing facilities; and other workplaces. Ongoing training and communication, capacity-building, and an emphasis on continuous improvement have been found to be the most effective path to sustaining progress toward greater compliance.</p>
+                <p>Social compliance policies must be backed with mechanisms to ensure leadership, employees, agents, and suppliers understand, apply, and adhere to the expected standard. Without providing proper guidance and training to supply chain partners, companies run the risk of higher compliance costs throughout their operations and supply chain. This lack of guidance and training may elevate the risk of child labor and forced labor entering corporate supply chains.</p>
+                <p>Many of the world&rsquo;s leading companies have developed robust communication strategies that disseminate the code, integrate the code into supplier contracts, and ensure comprehension. This is particularly important for companies that have multiple tiers of suppliers, as second-, third-, and fourth-tier suppliers often lack the material or institutional capacity to implement key provisions of a code.</p>
+                <p><a target="_blank" rel="noopener noreferrer" href="https://www.dol.gov/ilab/complychain/steps/4">Step 4: Communicate and Train Across Your Supply Chain</a> discusses the ins and outs of where, when, and how to communicate your code and compliance system to a variety of audiences: your own employees, your shareholders, your suppliers, supplier managers and supervisors, workers at facilities throughout the supply chain, governments, workers&rsquo; organizations, community groups, and others. It will also discuss ways to make the code more accessible to stakeholders by avoiding unnecessary technical jargon and providing for translation into multiple languages. Effective communication and training throughout the supply chain is key to building a culture of compliance.</p>
+                <p><em>* References to specific companies and non-governmental entities within this guide are for informational purposes only to demonstrate best practices that have been recognized internationally, as of the app&rsquo;s release date. Referencing the entities should not be interpreted as an official endorsement of those entities, their products, or services by the U.S. Department of Labor and their inclusion should not be interpreted as a comprehensive review of these entities&rsquo; practices in all areas.</em></p>
             </div>
         </StaticData>
     );
